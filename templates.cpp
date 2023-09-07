4474e9bfe251bfc6c57f4c06293dc504fd2ab45f
rao-rabi <rabirao1039@gmail.com>
2023-09-07 09:40:22 +0500

    first commit

--- templates.cpp
+++ templates.cpp
@@ -1,31 +1,39 @@
 #include<iostream>
 using namespace std;
 
-template <class T>
+// template<class T>
+template <class T, class V>
 
 class Vector{
     T a;
-    T b;
+    V b;
     public:
-    void setData(T x, T y){
+    void setData(T x, V y){
         a=x;
         b=y;
     };
     void getData(){
-        cout<<"The sum of all numbers is: "<<a + b <<endl;
+        // cout<<"The sum of all numbers is: "<<a + b <<endl;
+        cout<<"The statement is: "<<a<<" "<<b<<endl;
     };
 
 };
 int main(){
-    Vector <int>obj;
-    obj.setData(3,6);
+    // Vector <int>obj;
+    // obj.setData(3,6);
+    // obj.getData();
+    // Vector <float>obj2;
+    // obj2.setData(3.4,6.2);
+    // obj2.getData();
+    // Vector <double>obj3;
+    // obj3.setData(2.3,5.1);
+    // obj3.getData();
+    Vector <float, string> obj;
+    obj.setData(3.45,"Seconds ago");
     obj.getData();
-    Vector <float>obj2;
-    obj2.setData(3.4,6.2);
-    obj2.getData();
-    Vector <double>obj3;
-    obj3.setData(2.3,5.1);
-    obj3.getData();
+    Vector <int, char> obj1;
+    obj1.setData(3,'c');
+    obj1.getData();
 
 return 0;
  
